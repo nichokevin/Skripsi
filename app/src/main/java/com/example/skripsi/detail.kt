26f560046eb3d425e6b0904ed4881e8c32495d04
nichokevin <58069843+nichokevin@users.noreply.github.com>
2022-09-27 21:28:45 +0700

    Send data to another page

--- app/src/main/java/com/example/skripsi/detail.kt
+++ app/src/main/java/com/example/skripsi/detail.kt
@@ -1,8 +1,9 @@
 package com.example.skripsi
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
 
 class detail : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,5 +13,7 @@ class detail : AppCompatActivity() {
         supportActionBar?.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM;
         supportActionBar?.setCustomView(R.layout.action_bar_layout);
 
+        val sessionId = intent.getStringExtra("KEY_NAME")
+        Log.d("Testing send data",sessionId.toString())
     }
 }
--- app/src/main/java/com/example/skripsi/deteksi.kt
+++ app/src/main/java/com/example/skripsi/deteksi.kt
@@ -2,7 +2,6 @@ package com.example.skripsi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
 import android.widget.ToggleButton
 
 class deteksi : AppCompatActivity() {
