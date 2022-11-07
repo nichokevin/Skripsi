0b45c12eb3c649416911081d4b4e49a8202167d4
nichokevin <nicholaskevin28@gmail.com>
2022-11-07 20:46:43 +0700

    dark mode page detail

--- app/src/main/java/com/example/skripsi/detail.kt
+++ app/src/main/java/com/example/skripsi/detail.kt
@@ -1,15 +1,44 @@
 package com.example.skripsi
 
+import android.content.res.Configuration
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 
 class detail : AppCompatActivity() {
+
+    private fun isUsingNightModeResources(): Boolean {
+        return when (resources.configuration.uiMode and
+                Configuration.UI_MODE_NIGHT_MASK) {
+            Configuration.UI_MODE_NIGHT_YES -> true
+            Configuration.UI_MODE_NIGHT_NO -> false
+            Configuration.UI_MODE_NIGHT_UNDEFINED -> false
+            else -> false
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+
+        val cekTema = isUsingNightModeResources()
+        if (cekTema==true){
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            val actionBar: ActionBar?
+            actionBar = supportActionBar
+            val colorDrawable = ColorDrawable(Color.parseColor("#0F9D58"))
+
+            actionBar!!.setBackgroundDrawable(colorDrawable)
+            delegate.applyDayNight()
+        }else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            delegate.applyDayNight()
+        }
 
         supportActionBar?.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM;
         supportActionBar?.setCustomView(R.layout.action_bar_layout);
